6b8bac4e278751d68088d8724478c9370b21b044
Quentin Ikeno <ikenoq@gmail.com>
2022-07-02 12:16:12 -1000

    Make getNewCard async

--- src/CardDeck.js
+++ src/CardDeck.js
@@ -1,23 +1,25 @@
 import { useState, useEffect } from "react";
-import Card from './Card'
-import axios from "./axios";
+import Card from "./Card";
+import axios from "axios";
 
 const CardDeck = () => {
 	const baseUrl = "https://deckofcardsapi.com/api/deck/";
 	const [deckId, setDeckId] = useState(null);
 	const [cards, setCards] = useState([]);
 
-    const handleClick = () => {
-        const getNewCard = () => {
-            try {
-                const res = await axios.get(`${baseUrl}${deckId}/draw/?count=1`)
-                setCards(cards => [...cards, res.data.cards[0].image])
-            } catch (error) {
-                console.log(error)
-            }
-        }
-        getNewCard();
-    }
+	const handleClick = () => {
+		const getNewCard = async () => {
+			try {
+				const res = await axios.get(
+					`${baseUrl}${deckId}/draw/?count=1`
+				);
+				setCards((cards) => [...cards, res.data.cards[0].image]);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		getNewCard();
+	};
 
 	useEffect(() => {
 		const shuffleDeck = async () => {
@@ -33,13 +35,13 @@ const CardDeck = () => {
 		shuffleDeck();
 	}, []);
 
-    const cardComponents = cards.map(image => <Card src={image} />);
+	const cardComponents = cards.map((image) => <Card src={image} />);
 
 	return (
 		<div>
 			<h1>Deck of Cards</h1>
 			<button onClick={handleClick}>Gimme a Card!</button>
-            {cardComponents}
+			{cardComponents}
 		</div>
 	);
 };
